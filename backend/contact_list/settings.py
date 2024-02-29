b752111c3719022becbdae5d86674f79e9ec69de
Mateus Ceci <mateusceci@gmail.com>
2024-02-29 09:14:38 -0300

    update refresh token

--- backend/contact_list/settings.py
+++ backend/contact_list/settings.py
@@ -143,7 +143,7 @@ AUTH_USER_MODEL = 'contacts.CustomUser'
 from datetime import timedelta
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
