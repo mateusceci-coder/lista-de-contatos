2f7e8a4490591dd1e342ac0992345fd74e99391f
Mateus Ceci <mateusceci@gmail.com>
2024-03-01 15:33:00 -0300

    add jwt config

--- backend/contacts/urls.py
+++ backend/contacts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ContactViewSet, CreateUserView, LogoutView
+from .views import ContactViewSet, CreateUserView, LogoutView, MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
@@ -12,7 +11,7 @@ router.register(r'contacts', ContactViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', CreateUserView.as_view(), name='user_register'),
     path('logout/', LogoutView.as_view(), name='logout_user')
--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -8,14 +8,23 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.http import HttpResponse
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
 
-def create_user_view(request):
-    User = get_user_model()
-    user = User.objects.create(username='username')
-    user.set_password('password')
-    user.save()
-    return HttpResponse('User created successfully')
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
